c8cca0794bac14c7b02cb92f55e923745e1eaa97
Vincent254 <Vmwagiru@gmail.com>
2021-07-16 16:11:09 +0300

    Update emailtest.py

--- emailtest.py
+++ emailtest.py
@@ -14,8 +14,6 @@ message = Mail(
 try:
     print('=====================')
     load_dotenv('sendgrid.env')
-    print(os.environ.get('SENDGRID_API_KEY'))
-    print('=====================')
     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
     response = sg.send(message)
     print(response.status_code)
